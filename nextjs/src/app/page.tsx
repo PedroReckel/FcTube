a51fb7cec4bc267f7f160328dc3adda8838f53ce
PedroRekel <pedroreckel90@gmail.com>
2024-12-06 14:21:44 -0300

    Adicionado novos componentes a aplicação Next.js

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,17 +1,33 @@
-import { VideoCard } from "@/components/VideoCard";
 
-export default function Home() {
+import VideoCardSkeleton from "@/components/VideoCardSkeleton";
+import { VideosList } from "@/components/VideosList";
+import { Suspense } from "react";
+
+// const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+// async function getVideos(): Promise<VideoModel[]> {
+//   const response = await fetch('http://localhost:8000/api/videos', {
+//     // next: {
+//     //   revalidate: 10, // Eu posso ter um chache durante 10 segungos (diminuir as chamadas na API)
+//     // },
+//     cache: "no-store" // Não guardar cache (buscar tudo no servidor)
+//   });
+//   await sleep(2000) // Tornar a chamada na API mais lenta
+//   return response.json();
+// }
+
+export default async function Home({searchParams}: {searchParams: {search: string}}) {
   return (
     <div className="container mx-auto px-4 py-6">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      <VideoCard title="Video Title" thumbnail="/thumbnail.jpg" views={1000} likes={100}/>
-        <VideoCard title="Video Title" thumbnail="/thumbnail.jpg" views={1000} likes={100}/>
-        <VideoCard title="Video Title" thumbnail="/thumbnail.jpg" views={1000} likes={100}/>
-        <VideoCard title="Video Title" thumbnail="/thumbnail.jpg" views={1000} likes={100}/>
-        <VideoCard title="Video Title" thumbnail="/thumbnail.jpg" views={1000} likes={100}/>
-        <VideoCard title="Video Title" thumbnail="/thumbnail.jpg" views={1000} likes={100}/>
-        <VideoCard title="Video Title" thumbnail="/thumbnail.jpg" views={1000} likes={100}/>
-        <VideoCard title="Video Title" thumbnail="/thumbnail.jpg" views={1000} likes={100}/>
+        <Suspense
+          fallback={new Array(15).fill(null).map((_, index) => (
+            <VideoCardSkeleton key={index} />
+          ))}
+
+        >
+          <VideosList search={searchParams.search}/>
+        </Suspense>
       </div>
     </div>
   );
--- nextjs/src/components/Navbar/Navbar.tsx
+++ nextjs/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
+'use client'; // Eu preciso colocar o use client para identificar que é um 
+
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 // import { SearchBar } from "./SearchBar";
 
 export function Navbar() {
+  useSearchParams();
   return (
     <header className="bg-primary border-secondary border-b-2">
       <div className="container mx-auto px-4 py-2 flex items-center justify-between">
